4f04ba34346105bac89be719317cec413a8f5436
kstatua-alpenite <37293710+kstatua-alpenite@users.noreply.github.com>
2018-09-25 10:29:37 +0200

    Game1.js

---
+++ test3.js
@@ -0,0 +1,17 @@
+/* creazione minigioco */
+
+/* Titolo: blackJack JS - EASY
+ * Descrizione: Creazione di blackJack in javascript
+ * Regole del blackJack: 
+ *    STEP 1: 
+ *          il banchiere gira 2 (non un valore indefinito) carte e somma i loro valori con limite di 15.
+ *          Se la somma supera il 15 (non 21), il valore è 0.
+ *    STEP 2: 
+ *          il player gira 2 carte e somma i loro valori con limite di 15.
+ *          Se la somma supera il 15 (non 21), il valore è 0.
+ *    STEP 3:
+ *          Controllo vincitore, chi ha ottenuto il valore più vicino a 15 è il vincitore.
+ *          Se il banchiere e il player hanno lo stesso risultato, vince il banchiere 
+ *
+ */
+      
